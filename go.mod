fa3deae77a2cb442bee34dbfa3eeabf0eb771efe
Snow-00 <wariorwilly43@gmail.com>
2023-05-14 15:31:12 +0700

    finish learn go mod

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Snow-00/app-go-mod
 
 go 1.20
 
-require github.com/Snow-00/learn-go-module v1.0.2 // indirect
+require github.com/Snow-00/learn-go-module/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 /** 
 learn_go_module hanya alias
 nama package di module harus pake _ ga blh - dan nama package engga harus sama dengan nama dir nya itu sendiri
+
+kalo ada major changes yg merubah fitur (func) lama, hrs ubah nama module cth: .../v2
 */
+
 import ( 
 	"fmt"
-	learn_go_module "github.com/Snow-00/learn-go-module"
+	learn_go_module "github.com/Snow-00/learn-go-module/v2"
 )
 
 func main() {
-	fmt.Println(learn_go_module.SayHello())
+	fmt.Println(learn_go_module.SayHello("test"))
 }
